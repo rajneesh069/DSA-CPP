0fc1cc8b121f3c228156193bc35c30211d4e2f83
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-07-28 13:36:00 +0530

    do the CP setup in the CP folder

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    pair<int, int> p[3];
+    cout << p[0].first << endl;
+    cout << p[1].first << endl;
+    cout << p[2].first << endl;
+    cout << p[0].second << endl;
+    cout << p[1].second << endl;
+    cout << p[2].second << endl;
+
+    return 0;
+}
