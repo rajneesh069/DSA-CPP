befd87be185d8249481d44dfdb4e85cbfbc35aff
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-08-05 14:47:34 +0530

    check for palindrome

---
+++ DSA/recursion/is_palidrome.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_palindrome(string s, int i = 0) {
+    if (i >= s.size() / 2) {
+        return true;
+    }
+
+    return s[i] == s[s.size() - i - 1] && is_palindrome(s, i + 1);
+}
+
+int main() {
+
+    string s = "ABBA";
+    cout << is_palindrome(s) << endl;
+    
+    return 0;
+}
