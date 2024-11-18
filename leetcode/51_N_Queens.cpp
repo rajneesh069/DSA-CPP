c75060e33b98b09951e1253fe45b6b229f995792
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-11-19 00:29:19 +0530

    add the optimized code for N Queens

--- leetcode/51_N_Queens.cpp
+++ leetcode/51_N_Queens.cpp
@@ -55,6 +55,60 @@ public:
     }
 };
 
+// optimized code
+class Solution2 {
+private:
+    bool is_possible(int row, int col, vector<string>& board) {
+        int n = board.size();
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        for (int i = row + 1, j = col - 1; i < n && j >= 0; ++i, --j) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    void fn(vector<string>& board, int col, vector<vector<string>>& ans, int& n) {
+
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (is_possible(row, col, board)) {
+                board[row][col] = 'Q';
+                fn(board, col + 1, ans, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board;
+        for (int i = 0; i < n; i++) {
+            string s = "";
+
+            for (int j = 0; j < n; j++) {
+                s.push_back('.');
+            }
+
+            board.push_back(s);
+        }
+
+        vector<vector<string>>ans;
+        fn(board, 0, ans, n);
+        return ans;
+    }
+};
+
 int main() {
 
 
