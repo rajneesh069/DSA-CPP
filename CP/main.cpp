7593c8e4033a24c1d73fc732b685d71e55c34bfd
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-12-24 19:57:31 +0530

    CP removed from .gitignore

---
+++ CP/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 1 0 5 2 => 0 1 2 5
+// 1 0 5
+void fn(vector<int>& v, int el) {
+	if (v.empty() || v.back() <= el) {
+		v.push_back(el);
+		return;
+	}
+	int last = v.back();
+	v.pop_back();
+	fn(v, el);
+	v.push_back(last);
+}
+
+void sort_fn(vector<int>& v) {
+	if (v.size() == 1) return;
+	int el = v.back();
+	v.pop_back();
+	sort_fn(v);
+	fn(v, el);
+}
+
+int main() {
+	vector<int> v; int n;
+	cin >> n;
+	v.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	sort_fn(v);
+
+	for (auto& el : v) {
+		cout << el << " ";
+	}
+
+	return 0;
+}
