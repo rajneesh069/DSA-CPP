21f3f6c13e382e21cc5cfaa72759fe1fbb32cad7
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-12-25 14:06:29 +0530

    sort a stack using recursion done

---
+++ DSA/recursion/sort_a_stack.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+
+void fn(stack<int>&st, int el){
+	if(st.empty() || st.top()<=el){
+		st.push(el);
+		return;
+	}
+
+	int top = st.top();
+	st.pop();
+	fn(st, el); // 4 2 1
+	st.push(top);
+
+}
+
+void sort_stack(stack<int>&st){
+	if(st.size()==1){
+		return;
+	}
+ 	int el = st.top();
+	st.pop();
+	sort_stack(st);
+	fn(st, el);
+}
+
+void sortStack(stack<int> &stack)
+{
+	sort_stack(stack);
+}
