3df1de78a9df94079b5a960ca6292e9705d7b49a
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-01-21 22:20:23 +0530

    sort a stack.cpp added

--- DSA/recursion/sort_a_stack.cpp
+++ DSA/recursion/sort_a_stack.cpp
@@ -1,7 +1,9 @@
-#include <bits/stdc++.h> 
-
-void fn(stack<int>&st, int el){
-	if(st.empty() || st.top()<=el){
+#include <bits/stdc++.h>
+using namespace std;
+void fn(stack<int> &st, int el)
+{
+	if (st.empty() || st.top() <= el)
+	{
 		st.push(el);
 		return;
 	}
@@ -10,14 +12,15 @@ void fn(stack<int>&st, int el){
 	st.pop();
 	fn(st, el); // 4 2 1
 	st.push(top);
-
 }
 
-void sort_stack(stack<int>&st){
-	if(st.size()==1){
+void sort_stack(stack<int> &st)
+{
+	if (st.size() == 1)
+	{
 		return;
 	}
- 	int el = st.top();
+	int el = st.top();
 	st.pop();
 	sort_stack(st);
 	fn(st, el);
